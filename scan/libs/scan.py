1b09cf945a6f4732378a6cdd5c708d6385ecb0e9
Tony Wu <tony.wu@berkeley.edu>
2016-05-31 14:50:07 -0700

    Add value for opencv3

--- scan/libs/scan.py
+++ scan/libs/scan.py
@@ -8,6 +8,7 @@ from skimage.filter import threshold_adaptive
 import numpy as np
 import argparse
 import cv2
+import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -29,7 +30,7 @@ gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
 # show the original image and the edge detected image
-print "STEP 1: Edge Detection"
+print("STEP 1: Edge Detection")
 # cv2.imshow("Image", image)
 # cv2.imshow("Edged", edged)
 # cv2.waitKey(0)
@@ -37,7 +38,7 @@ print "STEP 1: Edge Detection"
 
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
-(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+image, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 screenCnt = None
 
@@ -64,7 +65,7 @@ if screenCnt is None:
 
 
 # show the contour (outline) of the piece of paper
-print "STEP 2: Find contours of paper"
+print("STEP 2: Find contours of paper")
 # cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 # cv2.imshow("Outline", image)
 # cv2.waitKey(0)
@@ -81,7 +82,13 @@ warped = threshold_adaptive(warped, 250, offset=10)
 warped = warped.astype("uint8") * 255
 
 # show the original and scanned images
-print "STEP 3: Apply perspective transform"
+print("STEP 3: Apply perspective transform")
 # cv2.imshow("Original", imutils.resize(orig, height=650))
 # cv2.imshow("Scanned", imutils.resize(warped, height=650))
 # cv2.waitKey(0)
+
+print("STEP 4: Writing image")
+filename = os.path.basename(args["image"])
+if not os.path.exists("results"):
+    os.mkdir("results")
+cv2.imwrite(os.path.join("results", filename), warped)
